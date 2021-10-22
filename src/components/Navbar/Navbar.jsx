2916bfba111b43c9833e51aabb6b661c55560da1
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-10-22 11:43:43 -0400

    updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,8 +17,8 @@ const Navbar = ({ totalItems }) => {
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit" >
-            <img  src={logo} alt="Commerce.js" height="25px" className={classes.image}/>
-            Commerce.js
+            <img  src={logo} alt="shopskie.js" height="25px" className={classes.image}/>
+            ShopSKIE.JS
           </Typography>
           <div className={classes.grow} />
           {location.pathname === '/' && (
