8e66d639242abec70d0ad97b02418fb98be1e663
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-10-23 10:59:41 -0400

    payment info all collected and working

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -13,6 +13,7 @@ const Checkout = ({ cart }) => {
 
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({});
   const classes = useStyles();
 
   useEffect(() => {
@@ -31,6 +32,17 @@ const Checkout = ({ cart }) => {
     generateToken();
   }, [cart]);
 
+
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  const next = (data) => {
+    setShippingData(data);
+
+    nextStep();
+  }
+
+
   const Confirmation = () => (
     <div>
       confirmation
@@ -38,8 +50,8 @@ const Checkout = ({ cart }) => {
   );
 
   const Form = () => (activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken} />
-    : <PaymentForm />
+    ? <AddressForm checkoutToken={checkoutToken} next={next}/>
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
   )
 
 
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextField, Grid } from '@material-ui/core';
 import { useForm, Controller } from 'react-hook-form';
 
-const FormInput = ({name, label, required}) => {
+const FormInput = ({name, label}) => {
   const { control } = useForm();
 
 
@@ -11,7 +11,7 @@ const FormInput = ({name, label, required}) => {
       <Controller 
         control={control}
         name={name}
-        required={required}
+        required
         render={({ field }) => <TextField fullWidth label={label} required />}
       />
     </Grid>
